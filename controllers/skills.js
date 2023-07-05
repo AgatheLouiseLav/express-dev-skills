21e01146efddb079a5dfb068ef25fbc5de649e04
Agathe Lavigne <gotgini@gmail.com>
2023-07-05 16:26:29 +0200

    add new skill page

--- controllers/skills.js
+++ controllers/skills.js
@@ -2,17 +2,24 @@ const Skills = require('../models/skill');
 
 module.exports = {
 	index,
-	show
-}
+	show,
+	new : newSkill
+};
+
+function newSkill(req, res) {
+	res.render('skills/new', {
+		title : 'New Dev Skill'
+	});
+};
 
 function index(req, res) {
 	res.render('skills/index', {
 		skills : Skills.getAll()
 	});
-}
+};
 
 function show(req, res) {
 	res.render('skills/show', {
 		skill: Skills.getOne(req.params.id)
 	});
-}
+};
